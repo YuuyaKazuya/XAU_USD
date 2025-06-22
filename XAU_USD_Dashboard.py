c09f9a2e1818a6b9f2750a4e31aecb4e8aab76ac
YuuyaKazuya <154810798+YuuyaKazuya@users.noreply.github.com>
2025-06-23 00:16:40 +0800

    Update XAU_USD_Dashboard.py

--- XAU_USD_Dashboard.py
+++ XAU_USD_Dashboard.py
@@ -40,6 +40,7 @@ if uploaded_file:
     df1 = pd.read_csv(uploaded_file)
     st.subheader("Uploaded Data")
     st.dataframe(df1.head())
+    st.dataframe(df1.tail())
 
     # Store the uploaded dataset in session_state for further processing
     st.session_state.df1 = df1
